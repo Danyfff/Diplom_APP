1517286ce77a962a00553a668d31efdd0d053918
dany <108307590+Danyfff@users.noreply.github.com>
2024-05-14 10:32:52 +0300

    create start script

---
+++ main.py
@@ -0,0 +1,12 @@
+from APP.db_scripts.user_scripts import user
+from PyQt6.QtWidgets import QApplication
+import sys
+from APP.login_form import Login
+
+
+
+if __name__ == '__main__': 
+    app = QApplication(sys.argv)
+    window = Login()
+    window.show()
+    app.exec()
